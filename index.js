5e25fa3ffa5c877512785540589d5f5d0c21206a
Rajkumar Palani <opensesame@gmail.com>
2019-12-23 15:14:36 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+const Person = require('./person');
+
+const p1 = new Person('John Doe', 40);
+
+p1.greeting();
+
+
---
+++ ref/http_demo.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+//create server Object
+http.createServer((req, res) => {
+    //Write Response
+    res.write('Hello World');
+    res.end();
+}).listen(5000, ()=> console.log('Server Running....!'));
+
+
